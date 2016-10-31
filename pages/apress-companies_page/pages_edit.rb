0cb378d549b32549fd3b01584c86ec2845b873e6
Hardboy1208 <birbrover@gmail.com>
2016-10-31 11:13:08 +0500

    add case: check entry point
    add methods to_be and not_to_be
    
    remove unnecessary require

--- pages/apress-companies_page/pages_edit.rb
+++ pages/apress-companies_page/pages_edit.rb
@@ -1,5 +1,24 @@
 class PagesEdit
-  include PageObject
+  #include PageObject # Без комментирования это строки код не работает
 
+  def to_be(driver,xpath,where)
+
+    if driver.find_element(:xpath, xpath) == nil
+      raise 'My Exception'
+    else
+      puts '  check in '+where+' ok'
+    end
+
+  end
+
+  def not_to_be(driver,xpath,where)
+
+    if driver.find_element(:xpath, xpath) == nil
+      puts '  check in '+where+' ok'
+    else
+      raise 'My Exception'
+    end
+
+  end
 
 end
