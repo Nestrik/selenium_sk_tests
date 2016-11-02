96d1773485703fbbc5ed8f4c97d0fce5acb4a188
Hardboy1208 <birbrover@gmail.com>
2016-11-02 18:53:15 +0500

    refactor in ruby style

--- pages/apress-companies_page/pages_edit.rb
+++ pages/apress-companies_page/pages_edit.rb
@@ -1,24 +1,3 @@
 class PagesEdit
-  #include PageObject # Без комментирования это строки код не работает
-
-  def to_be(driver,xpath,where)
-
-    if driver.find_element(:xpath, xpath) == nil
-      raise 'My Exception'
-    else
-      puts '  check in '+where+' ok'
-    end
-
-  end
-
-  def not_to_be(driver,xpath,where)
-
-    if driver.find_element(:xpath, xpath) == nil
-      puts '  check in '+where+' ok'
-    else
-      raise 'My Exception'
-    end
-
-  end
 
 end
--- spec/apress-companies_page_spec/pages_spec.rb
+++ spec/apress-companies_page_spec/pages_spec.rb
@@ -3,33 +3,64 @@ require_relative '../../pages/apress-companies_page/pages_edit'
 
 describe 'Page edit' do
 
-  before :all do
+  let(:user_auth_admin) { Page::CONFIG['super_user_enter_url'].to_s }
+  let(:user_auth_without_sk) { Page::CONFIG['normal_user_enter_url'].to_s }
+  let(:sk_page) { Page::CONFIG['sk_page'].to_s }
+  let(:check_xpath) { PagesEdit.new }
+  let(:setting_toolbar) { @driver.find_element(:xpath, "//div[text()='Настройки']") }
+
+  before :context do
     @driver = start_browser
-    @super_user_auth = Page::CONFIG['super_user_enter_url'].to_s
-    @normal_user_auth = Page::CONFIG['normal_user_enter_url'].to_s
-    @sk_page = Page::CONFIG['sk_page'].to_s
-    @check_xpath = PagesEdit.new
   end
 
-  after(:all) do
+  after :context do
     @driver.quit
   end
 
-  it 'check entry point with SU' do
-    @driver.get @super_user_auth
-    @driver.get @sk_page
-    @setting_toolbar = @driver.find_element(:xpath, "//div[text()='Настройки']")
-    @setting_toolbar.click
-    @check_xpath.to_be(@driver, "//a[text()='Добавить новую страницу']", "toolbar")
-    @check_xpath.to_be(@driver, "//a[text()='+ Добавить страницу']", "left menu")
+  context 'Check exist of entry point while role SU' do
+
+    before{
+      @driver.get user_auth_admin
+      @driver.get sk_page
+    }
+
+    it 'in setting' do
+      setting_toolbar.click
+      expect(@driver.find_element(:xpath, "//a[text()='Добавить новую страницу']").displayed?).to be true
+    end
+
+    it 'in left menu' do
+      expect(@driver.find_element(:xpath, "//a[text()='+ Добавить страницу']").displayed?).to be true
+    end
+
   end
 
-  it 'check entry point without SU' do
-    @driver.get @normal_user_auth
-    @driver.get @sk_page
-    @check_xpath.not_to_be(@driver, "//div[text()='Настройки']", "Настройки")
-    @check_xpath.not_to_be(@driver, "//a[text()='Добавить новую страницу']", "toolbar")
-    @check_xpath.not_to_be(@driver, "//a[text()='+ Добавить страницу']", "left menu")
+  context 'Check exist of entry point while role authorized user without sk' do
+
+    before{
+      @driver.get user_auth_without_sk
+      @driver.get sk_page
+    }
+
+    it 'button setting' do
+      expect(@driver.find_element(:xpath, "div[text()='Настройки']")).to be nil
+    end
+
+    it 'in setting' do
+      expect(@driver.find_element(:xpath, "//a[text()='Добавить новую страницу']")).to be nil
+    end
+
+    it 'in left menu' do
+      expect(@driver.find_element(:xpath, "//a[text()='+ Добавить страницу']")).to be nil
+    end
+
+  end
+
+  it 'check HTML elements' do
+    @driver.get user_auth_admin
+    @driver.get sk_page+"pages/new"
+
+    #TODO проверки title, header и т.д.
   end
 
 end
